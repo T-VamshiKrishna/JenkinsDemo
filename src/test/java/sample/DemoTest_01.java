1b355c256e2a86fee9b8f31c4f5e2f7fd232fa65
T VamshiKrishna <vamshi.cpl@gmail.com>
2021-09-08 17:10:13 +0530

    Created Tests for JenkinsDemo

---
+++ src/test/java/sample/DemoTest_01.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest_01 {
+	
+	@Test
+	public void testcase_001(){
+		System.out.println("This is test case_001");
+	}
+	
+	@Test
+	public void testcase_002(){
+		System.out.println("This is test case_002");
+	}
+	
+	@Test
+	public void testcase_003(){
+		System.out.println("This is test case_003");
+	}
+
+}
---
+++ src/test/java/sample/DemoTest_02.java
@@ -0,0 +1,21 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest_02 {
+	@Test
+	public void testcase_004(){
+		System.out.println("This is test case_004");
+	}
+	
+	@Test
+	public void testcase_005(){
+		System.out.println("This is test case_005");
+	}
+	
+	@Test
+	public void testcase_006(){
+		System.out.println("This is test case_006");
+	}
+
+}
